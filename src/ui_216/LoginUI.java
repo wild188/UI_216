135f206519c254529b23a6ca5677485109acc01b
Billy DeLucia <wild188@gmail.com>
2016-03-02 18:38:31 -0500

    made main method private in login UI

--- src/ui_216/LoginUI.java
+++ src/ui_216/LoginUI.java
@@ -130,7 +130,7 @@ public class LoginUI extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    private static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -162,7 +162,7 @@ public class LoginUI extends javax.swing.JFrame {
         });
     }
     
-    public void replaceMain(String args[]) {
+    private void replaceMain(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
