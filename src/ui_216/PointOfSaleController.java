8191508350efcd3277293cb621f5cadb74c87656
Billy DeLucia <wild188@gmail.com>
2016-03-06 19:15:24 -0500

    adding methods to posController

--- src/ui_216/PointOfSaleController.java
+++ src/ui_216/PointOfSaleController.java
@@ -14,6 +14,7 @@ public class PointOfSaleController {
     private PointOfSaleUI pos;          //data fields to be passed to the session
     private String userID;
     private String password; 
+    private PurchaseSession model;
     
     public PointOfSaleController(String id, String pass){
         userID = id;
@@ -21,4 +22,29 @@ public class PointOfSaleController {
         pos = new PointOfSaleUI(this);
         pos.setVisible(true);            //start POS ui
     }
+    
+    public void addItem(String itemIDString){
+        Integer id;
+        Triplet itemInfo = null;
+        Item item = null;
+        Integer quantity = 0;
+        if((id = Integer.getInteger(itemIDString)) == null){
+            pos.displayError("Item ID invalid, not a number");
+        }else{
+            //tripple a = item b = in quant c = purchase type
+            itemInfo = model.addItem();
+            item = itemInfo.getA();
+            quantity = item.Info.getB();
+            // get item purchase type
+            pos.addLineItem(item.getID().toString(), item.getName(), item.getPrice().toString(), quantity.toString());
+        }
+        
+        
+        
+    }
+    
+    public void removeItem(int itemID){
+        
+    }
+    
 }
